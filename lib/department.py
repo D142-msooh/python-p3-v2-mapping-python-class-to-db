fd357a45f102d5127d573c80ef205e744437638e
D142-msooh <kinyajuidama176@gmail.com>
2025-11-29 22:00:22 +0300

    Add department.py

--- lib/department.py
+++ lib/department.py
@@ -13,4 +13,49 @@ class Department:
     
     @classmethod
     def create_table(cls):
-        
+        sql = """
+            CREATE TABLE IF NOT EXISTS departments (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                location TEXT
+            )
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        sql = "DROP TABLE IF EXISTS departments"
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    def save(self):
+        sql = """
+            INSERT INTO departments (name, location)
+            VALUES (?, ?)
+        """
+        CURSOR.execute(sql, (self.name, self.location))
+        CONN.commit()
+        self.id = CURSOR.lastrowid
+
+    @classmethod
+    def create(cls, name, location):
+        dept = cls(name, location)
+        dept.save()
+        return dept
+
+    def update(self):
+        sql = """
+            UPDATE departments
+            SET name = ?, location = ?
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.name, self.location, self.id))
+        CONN.commit()
+
+    def delete(self):
+        sql = "DELETE FROM departments WHERE id = ?"
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
+
+
